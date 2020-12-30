bff8433b4429cfffd386e3141764e9a41c6e30e2
Romans Sinicins <romans@sinicins.com>
2020-12-30 08:30:14 +0200

    declaration and implementation files for class Geometry

---
+++ Geometry.cpp
@@ -0,0 +1,38 @@
+//  Geometry.cpp
+//  Class Declaration File
+//  to store the class implementation
+
+#include "Geometry.hpp"
+#include <iostream>
+
+// constructor
+Geometry::Geometry()
+{
+    // some code for constructor...
+    // if needed
+}
+
+// destructor
+Geometry::~Geometry()
+{
+    // some code for destructor...
+    // if needed
+}
+
+// methods
+void Geometry::SomeMethod1()
+{
+    // some code for method1...
+}
+
+void Geometry::SomeMethod2()
+{
+    // some code for method2...
+}
+
+void Geometry::SomeMethod3()
+{
+    // some code for method3...
+}
+
+// ...
---
+++ Geometry.hpp
@@ -0,0 +1,32 @@
+//  Geometry.hpp
+//  Class Implementation File
+//  to store the class declaration
+
+#ifndef Geometry_hpp
+#define Geometry_hpp
+
+#include <iostream>
+
+class Geometry
+{
+private:
+    // some data, variables etc
+    // int a;
+    // bool b;
+    // string c;
+    
+public:
+    // constructor
+    Geometry();
+    
+    // destructor
+    ~Geometry();
+    
+    // methods
+    void SomeMethod1();
+    void SomeMethod2();
+    void SomeMethod3();
+    // ...
+};
+
+#endif /* Geometry_hpp */
